9aab5ff3a817e5d1104d5c682583da704dff46a9
Gabriel Model <gabrielmedeirosmodel@gmail.com>
2025-06-10 21:09:31 -0300

    java2

---
+++ javagaby/atividade2.js
@@ -0,0 +1,38 @@
+//atividade 1
+// var ListadeFrutas = ['Maçã', 'Uvas' , 'Banana' , 'Abacaxi' , 'Melancia'];
+// ListadeFrutas.push("Pera");
+// ListadeFrutas.push("Coco");
+// for (let i = 0; i < ListadeFrutas.length; i++) {
+//   console.log("Item da lista:", ListadeFrutas[i]);
+// }
+
+//atividade 2
+// var Num = ['1' , '2' , '3' , '4' , '5' , '6' , '7', '8' , '9', '10'];
+// for (let i = 0; i < Num.length; i++) {
+//   console.log("Item da lista:", Num[i]);
+// }
+
+//atividade 3
+// var Num 
+// let Num_while = 10;
+// while (Num_while > 0) {
+//     console.log("Número:", Num_while);
+//     Num_while--;
+// }
+
+//atividade 4
+// let numeros = [2 , 3 , 5, 8, 10];
+// var soma = 0
+// for (let i = 0; i < numeros.length; i++) {
+//   soma = numeros[i] + soma;
+// }
+// console.log(soma);
+
+
+//atividade 5
+// let letras_join = ["ana", "bruno", "carlos" , "joao"];
+// let resultado_join = letras_join.join("; ");
+// console.log(resultado_join);
+
+//atividade 6
+
